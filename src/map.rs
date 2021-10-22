bcbb9f5e6f14e968fd51f8b388a1937335cf5c07
t56k <txnash@gmail.com>
2021-10-22 17:01:05 +1300

    change wall symbol

--- src/map.rs
+++ src/map.rs
@@ -50,7 +50,7 @@ impl Map {
                         ctx.set(x, y, YELLOW, BLACK, to_cp437(','));
                     }
                     TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, to_cp437('.'));
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
                     }
                 }
             }
