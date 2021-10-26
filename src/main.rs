6017a661e72473144449b96616b07b3fe4597c97
t56k <txnash@gmail.com>
2021-10-26 16:07:54 +1300

    add room architect and empty architect

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,9 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
         spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
+        map_builder.monster_spawns
+            .iter()
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, *pos));
         map_builder
             .rooms
             .iter()
@@ -96,6 +99,9 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut self.ecs, map_builder.player_start);
         spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        map_builder.monster_spawns
+            .iter()
+            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, *pos));
         map_builder.rooms
             .iter()
             .skip(1)
