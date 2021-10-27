dd3cec9e61711f4e8c3a8ceb6f697af19cec6f72
t56k <txnash@gmail.com>
2021-10-28 08:52:45 +1300

    cleanup

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -55,7 +55,6 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
-        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -65,7 +64,6 @@ pub fn player_input(
                 })
                 .for_each(|(entity, _)| {
                     hit_something = true;
-                    did_something = true;
 
                     commands
                         .push(((), WantsToAttack {
@@ -75,7 +73,6 @@ pub fn player_input(
                 });
 
             if !hit_something {
-                did_something = true;
                 commands
                     .push(((), WantsToMove {
                         entity: player_entity,
@@ -83,15 +80,7 @@ pub fn player_input(
                     }));
             }
         }
-        if !did_something {
-            if let Ok(mut health) = ecs
-                .entry_mut(player_entity)
-                .unwrap()
-                .get_component_mut::<Health>()
-            {
-                health.current = i32::min(health.max, health.current + 1);
-            }
-        }
+
         *turn_state = TurnState::PlayerTurn;
     }
 }
