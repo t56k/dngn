b7abe4bbddbe3c0f19271031f082a465d64186a4
t56k <txnash@gmail.com>
2021-10-27 13:06:17 +1300

    spawn objects

--- src/spawner.rs
+++ src/spawner.rs
@@ -20,6 +20,48 @@ pub fn spawn_player(
     );
 }
 
+pub fn spawn_entity(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point
+) {
+    let roll = rng.roll_dice(1, 6);
+    match roll {
+        1 => spawn_healing_potion(ecs, pos),
+        2 => spawn_magic_mapper(ecs, pos),
+        _ => spawn_monster(ecs, rng, pos)
+    }
+
+}
+
+pub fn spawn_healing_potion(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (Item,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('!')
+            },
+            Name("Healing Potion".to_string()),
+            ProvidesHealing {amount: 6}
+        )
+    );
+}
+
+pub fn spawn_magic_mapper(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (Item,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('{')
+            },
+            Name("Dungeon Map".to_string()),
+            ProvidesDungeonMap {}
+        )
+    );
+}
+
 pub fn spawn_monster(
     ecs: &mut World,
     rng: &mut RandomNumberGenerator,
